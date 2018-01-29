a7e2c515b5ee3a3da34978220191b4652b4bec62
Paweł Mucha <pawel.mucha@yahoo.pl>
2018-01-29 23:06:11 +0100

    Save scan data to .mat file

--- controllers/scanning.py
+++ controllers/scanning.py
@@ -11,27 +11,27 @@ from models.scanning import ScanResultMat
 def scan_result(id):
     result = ScanResultMat.get(id)
     mat_contents = scipy.io.loadmat('data/{}'.format(result.mat_filename))
-    x = mat_contents['x'].flatten()
-    y = mat_contents['y'].flatten()
-    z = mat_contents['z'].flatten()
-    f = mat_contents['f'].flatten()
+    x = mat_contents['x'].flatten().tolist()
+    y = mat_contents['y'].flatten().tolist()
+    z = mat_contents['z'].flatten().tolist()
+    f = mat_contents['f'].flatten().tolist()
     e = mat_contents['E']
 
-    print(request.query_params)
     q = request.query_params
     start_z = int(q.get('sz', 0))
-    end_z = int(q.get('ez', z.size - 1))
+    end_z = int(q.get('ez', len(z) - 1))
     start_f = int(q.get('sf', 0))
-    end_f = int(q.get('ef', f.size - 1))
+    end_f = int(q.get('ef', len(f) - 1))
 
-    sliced = e[:, :, start_z:end_z, start_f:end_f]
+    sliced = e[:, :, start_z:end_z + 1, start_f:end_f + 1]
+    print(x)
 
     reduced = scipy.add.reduce(sliced, axis=(2, 3,))
 
     return {
-        'x': x.tolist(),
-        'y': y.tolist(),
-        'z': z.tolist(),
-        'f': f.tolist(),
+        'x': x,
+        'y': y,
+        'z': z,
+        'f': f,
         'e': reduced.tolist()
     }
--- tasks.py
+++ tasks.py
@@ -21,29 +21,53 @@ def initialize_database():
     return db
 
 
-def save_scan_result(scan):
-    x, y, z, f = [], [], [], []
-    e = []
+def save_scan_result(scan_id):
+    x_set, y_set, z_set, f_set = (set() for i in range(4))  # multiple sets
+    e_list = []
 
-    files = os.listdir('data/{}'.format(scan.id))
+    dir_path = 'data/{}'.format(scan_id)
+    files = os.listdir(dir_path)
     for filename in files:
         mesurement_no, coords_and_extension = filename.split('-')
         coords = coords_and_extension[:-4]
-        x, y, z = coords.split('_')
-        f = open(filename)
+        x, y, z = [int(coord) for coord in coords.split('_')]
+        x_set.add(x)
+        y_set.add(y)
+        z_set.add(z)
+        f = open(os.path.join(dir_path, filename))
         lines = f.readlines()[28:]
         for line in lines:
-            frequency, max, min = line.split(';')
+            frequency, max_e, min_e = [float(num) for num in line.split(';')]
+            f_set.add(frequency)
+            e_list.append({'x': x, 'y': y, 'z': z, 'f': frequency, 'e': max_e})
+        f.close()
 
+    e_sci = scipy.zeros((len(x_set), len(y_set), len(z_set), len(f_set)))  # destined 4 dimensional E matrix
+    xs = sorted(x_set)
+    ys = sorted(y_set)
+    zs = sorted(z_set)
+    fs = sorted(f_set)
+    for e in e_list:
+        x_index = xs.index(e['x'])
+        y_index = ys.index(e['y'])
+        z_index = zs.index(e['z'])
+        f_index = fs.index(e['f'])
+        e_sci[x_index, y_index, z_index, f_index] = e['e']
+
+    output_dict = {'x': xs, 'y': ys, 'z': zs, 'f': fs, 'E': e_sci}
+    mat_file = '{}.mat'.format(scan_id)
+    scipy.io.savemat('data/{}'.format(mat_file), output_dict)
 
     # Here should be code for parsing data from spectrum analyzer and saving it as .mat file
-    sr = ScanResultMat.create(mat_filename='NFS.mat', scan=scan.id)
-    print(sr)
+    db = initialize_database()
+    with db.connection:
+        ScanResultMat.create(mat_filename=mat_file, scan=scan_id)
+        db.commit()
 
 
 class ScanerObserver():
 
-    def __init__(self, scan_id) -> None:
+    def __init__(self, scan_id):
         self.scan_id = scan_id
         super().__init__()
 
@@ -60,17 +84,7 @@ class ScanerObserver():
 
 @celer.task
 def increase_progress(scan_id):
-    db = initialize_database()
     scan_observer = ScanerObserver(scan_id)
     nfs = skaner.NFS(observer=scan_observer)
     nfs.scan('emc_scanner/rpi0_res10.gcode', scan_id=scan_id)
-    # with db.connection():
-    #     scan = Scan.get(scan_id)
-    #     while scan.progress != 80:
-    #         scan.update_record(progress=scan.progress + 10)
-    #         db.commit()
-    #         sleep(1)
-    #
-    #     save_scan_result(scan)
-    #     scan.update_record(progress=100, status='finished')
-    #     db.commit()
+    save_scan_result(scan_id)
